3eb4265ff7518db2d6207261446c05c156f758f6
khoi.tuan <khoi.tuan@ladbrokes.com.au>
2022-05-28 11:23:45 +1000

    feat(go-kit_kit): add stringsvc1

---
+++ go-kit_kit/README.md
@@ -0,0 +1,3 @@
+# go-kit/kit
+
+I followed the official documentation's examples: [https://gokit.io/examples/](https://gokit.io/examples/)
---
+++ go-kit_kit/stringsvc1/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+/* ----------------------------- Business Logic ----------------------------- */
+// In Go kit, we model a service as an interface
+// StringService provides operations on strings.
+type StringService interface {
+	Uppercase(string) (string, error)
+	Count(string) int
+}
+
+type stringService struct{}
+
+func (stringService) Uppercase(s string) (string, error) {
+	if s == "" {
+		return "", ErrEmpty
+	}
+	return strings.ToUpper(s), nil
+}
+
+func (stringService) Count(s string) int {
+	return len(s)
+}
+
+// ErrEmpty is returned when input string is empty.
+var ErrEmpty = errors.New("empty string")
+
+/* ------------------------- Requests and Responses ------------------------- */
+// In Go kit, the primary messaging pattern is RPC. So every method in our interface will be modelled as as a remote procedure call (RPC)
+// For each method, we define request and response structs, capturing all of the input and output parameters
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V   string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
+
+/* -------------------------------- Endpoints ------------------------------- */
+// type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+
+// It represents a single RPC, a single method in our service interface
+// We'll write simple adapters to convert each of our service's methods into an endpoint.
+// Each adapter takes a StringService, and returns an endpoint that corresponds to one of the methods
+
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{v, ""}, nil
+	}
+}
+
+func makeCountEndpoint(svc StringService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(countRequest)
+		v := svc.Count(req.S)
+		return countResponse{v}, nil
+	}
+}
+
+/* ------------------------------- Transports ------------------------------- */
+// Now we need to expose the service to the outside world
+// Your organization probably already has opinions about how services should talk to each other
+// Go kit supports many transports out of the box
+// For this minimal example service, let's use JSON over HTTP. Go kit provides a helper struct, in package transport/http
+
+func main() {
+	svc := stringService{}
+
+	uppercaseHandler := httptransport.NewServer(
+		makeUppercaseEndpoint(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request uppercaseRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request countRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
